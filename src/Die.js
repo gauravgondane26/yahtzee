9f687852eeb9c254bdb95c2be850cd4f21d49b94
gauravgondane26 <gauravgondane1@gmail.com>
2024-05-14 21:39:52 -0400

    initial commit

---
+++ src/Die.js
@@ -0,0 +1,29 @@
+import React, { Component } from "react";
+import "./Die.css";
+
+class Die extends Component {
+  static defaultProps = {
+    numWords: ["one", "two", "three", "four", "five", "six"],
+    val: 5
+  }
+  constructor(props) {
+    super(props);
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+  handleClick() {
+    this.props.handleClick(this.props.idx);
+  }
+  
+  render() {
+    const {numWords, locked, val, disabled, rolling} = this.props;
+    let classes = `Die fas fa-dice-${numWords[val - 1]} fa-5x`;
+    if(locked) classes += " Die-locked";
+    if(rolling) classes += " Die-rolling";
+    return (
+      <i className={classes} onClick={this.handleClick} disabled={disabled} />
+    );
+  }
+}
+
+export default Die;
